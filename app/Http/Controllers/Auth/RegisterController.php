f21ce21794066ceda2abb260a41a4daa57d691f4
Jonathan Castro <jonathan.castro@whitejaguars.com>
2021-08-12 19:24:13 -0600

    Privileges + Register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'motherLastName' => ['required', 'string', 'max:255'],
+            'birthDate' => ['required', 'date'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -66,6 +69,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'lastName' => $data['lastName'],
+            'motherLastName' => $data['motherLastName'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastName',
+        'motherLastName',
+        'birthDate',
         'email',
         'password',
     ];
